b5ee0686485229a26a204ff87b5b6dd11c5599cc
Fatma Zaman <fatma.zaman@verizonwireless.com>
2017-03-30 14:31:22 -0400

    Front end(html) is working

--- Movies.py
+++ Movies.py
@@ -17,12 +17,11 @@ mad_max = media.Movie("Mad Max", "Fury Road",
 	                    "https://www.youtube.com/watch?v=woHTUsl66BY")
 imitation = media.Movie("The Imitation Game","Real life story of legendary cryptanalyst Alan Turing",
 							"http://ia.media-imdb.com/images/M/MV5BNDkwNTEyMzkzNl5BMl5BanBnXkFtZTgwNTAwNzk3MjE@._V1_SX640_SY720_.jpg",
-							"https://www.youtube.com/watch?v=Sn-7SNrQWMo") 
+							"https://www.youtube.com/watch?v=Sn-7SNrQWMo")
 
 gone_girl = media.Movie("Gone Girl","Did Nick Dunne kill his wife? ",
 							"http://ia.media-imdb.com/images/M/MV5BMTk0MDQ3MzAzOV5BMl5BanBnXkFtZTgwNzU1NzE3MjE@._V1_SX640_SY720_.jpg",
 							"https://www.youtube.com/watch?v=Ym3LB0lOJ0o")
-movies = ["toy_story","avatar","avenger","mad_max","imitation","gone_girl"]
+movies = [toy_story,avatar,avenger,mad_max,imitation,gone_girl]
+
 fresh_tomatoes.open_movies_page(movies)
-
-
